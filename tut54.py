6707bd478d0197cfab5acc6bac159aaf515e6ec1
rowince <rowincerock@gmail.com>
2022-12-24 11:18:00 +0530

    code added

---
+++ tut54.py
@@ -0,0 +1,54 @@
+import string
+# sort string
+# input_data = '2name 1my 4rowince 3is'
+# output = 'my name is rowince'
+
+input_data = '2name 1my 4rowince 3is'
+
+# for function
+
+
+# def name(name_str):
+#     x = name_str.split()
+#     x.sort()
+#     temp = []
+#     for i in x:
+#         new = ''.join([j for j in i if j.isalpha()])
+#         temp.append(new)
+#     new = ' '.join(temp)
+#     print(new)
+
+# def name(name_str):
+#     x = name_str.split()
+#     x.sort()
+#     temp = []
+#     for i in x:
+#         temp.append(i.translate(str.maketrans('', '', string.digits)))
+#     new = ' '.join(temp)
+#     print(new)
+
+
+# name(input_data)
+
+# for loop
+# x = input_data.split()
+# x.sort()
+# temp = []
+# for i in x:
+#     temp_new = []
+#     temp_new.extend(i)
+#     str_j = ''
+#     for j in temp_new:
+#         if j.isalpha():
+#             str_j = str_j + j
+#     temp.append(str_j)
+# y = ' '.join(temp)
+# print(y)
+
+# remove integer from list
+# x = '1my'
+
+# new = str.maketrans('', '', string.digits)
+# new_str = x.translate(new)
+# new_str = x.translate(str.maketrans('', '', string.digits))
+# print(new_str)
---
+++ tut55.py
@@ -0,0 +1,24 @@
+# sort method
+
+# x = ['30', '22', '11', '2', '1', '9']
+# x.sort()
+# x = sorted(x)
+# print(x)
+
+# y = ['30', '22', '11', '2', '1', '9']
+# y.sort(key=int)
+# y = sorted(y, key=int)
+# print(y)
+
+# z = ['name', 'my', 'rowince', 'is']
+# z.sort()
+# print(z)
+
+# k = ['2name', '1my', '4rowince', '3is']
+# k.sort()
+# print(k)
+
+f = (10, 1)
+# f.sort()
+# f = sorted(f)
+print(f)
